a7bee20f95f2a9fdfbfab8f8b2a7d211f2830afe
DanLangas <dan.langas@gmail.com>
2021-02-02 23:23:16 +0200

    automation1.py

---
+++ os_automation/automation1.py
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Name:        automation
+# Purpose:
+#
+# Author:      dan
+#
+# Created:     02/02/2021
+# Copyright:   (c) dan 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+inport os
+from datetime import date
+import pandas as pd
+
+data_location = 'Docs/Receipts'
+file_list = []
+for file in os.listir(data_location):
+    file_list.append(file)
+
+data = {'file_name' : file_list }
+file_df = pd.DataFrame(data)
+new_file_directory = 'Docs/Processed'
+today = date.today()
+file_df.to_excel(new_file_directory + 'receipts_sum -' + str(today) + '.xlsx' )
+
+for file in os.listdir(data_location):
+    os.rename(data_location + file, new_file_directory + file )
+
+string_to_find = 'Dan'
+directory_to_search = 'Docs/Processe'
+dan_docs = []
+for file in os.listdir(directory_to_search):
+    with open(directory_to_search + file) as f:
+        dan_docs.append(file)
+
+print(dan_docs)
